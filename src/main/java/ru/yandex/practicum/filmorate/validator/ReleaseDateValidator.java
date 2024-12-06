56440239b0e82eaacbf3846b245b76d07e456b9b
Oleg <bzbms@ya.ru>
2024-12-06 11:17:23 +0300

    ТЗ-10. Добавление Film и User. Константу дооформил.

--- src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidator.java
@@ -6,7 +6,7 @@ import jakarta.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 
 public class ReleaseDateValidator implements ConstraintValidator<ReleaseDate, LocalDate> {
-    private final LocalDate BIRTHDAY_OF_CINEMA = LocalDate.of(1895, 12, 28);
+    private static final LocalDate BIRTHDAY_OF_CINEMA = LocalDate.of(1895, 12, 28);
 
     @Override
     public boolean isValid(LocalDate releaseDate, ConstraintValidatorContext context) {
